a78ca681dd2cf5b6f61980517df48e16d41cee46
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-07-14 15:59:47 -0500

    getting code back from callback, added retrofit2 moshi ok http to grade

--- app/src/main/java/com/example/cryptowallet/MainActivity.kt
+++ app/src/main/java/com/example/cryptowallet/MainActivity.kt
@@ -1,126 +1,42 @@
 package com.example.cryptowallet
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.core.net.toUri
-
-import net.openid.appauth.AuthorizationServiceConfiguration
-import net.openid.appauth.AuthorizationServiceConfiguration.RetrieveConfigurationCallback
-import net.openid.appauth.ResponseTypeValues
-
-import net.openid.appauth.AuthorizationRequest
-import android.content.Intent
-
-import android.app.PendingIntent
-import android.os.PersistableBundle
-import android.widget.Button
-
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationException
-
-import net.openid.appauth.AuthorizationResponse
-import net.openid.appauth.AuthorizationService.TokenResponseCallback
-import net.openid.appauth.AuthState
-import net.openid.appauth.AuthState.AuthStateAction
-import androidx.core.app.ActivityCompat.startActivityForResult
-import net.openid.appauth.TokenResponse
-
-
-
-
-
-
+import android.view.LayoutInflater
+import com.example.cryptowallet.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val MY_CLIENT_ID = "e4faf6ec45843a2f1e8a42c6242f3d8e82ce5603d3ee9c86c85be29a6361104f"
-        val MY_REDIRECT_URI = "com.example.cryptowallet://coinbase-oauth".toUri()
+        val MY_REDIRECT_URI = "cryptowallet://callback"
     }
-    lateinit var serviceConfig:AuthorizationServiceConfiguration
-    lateinit var authService:AuthorizationService
-    lateinit var authState: AuthState
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val inflater = LayoutInflater.from(this)
+        val binding = ActivityMainBinding.inflate(inflater)
+        setContentView(binding.root)
 
-        serviceConfig = AuthorizationServiceConfiguration(
-            Uri.parse("https://www.coinbase.com/oauth/authorize"),  // authorization endpoint
-            Uri.parse("http://www.coinbase.com/oauth/token")
-        ) // token endpoint
-
-/*
-        AuthorizationServiceConfiguration.fetchFromIssuer(
-            Uri.parse("https://www.coinbase.com/oauth/authorize?client_id=e4faf6ec45843a2f1e8a42c6242f3d8e82ce5603d3ee9c86c85be29a6361104f&redirect_uri=cryptoWallet%3A%2F%2Fcoinbase-oauth&response_type=code&scope=wallet%3Auser%3Aread"),
-            RetrieveConfigurationCallback { serviceConfiguration, ex ->
-                if (ex != null) {
-                    Log.e("AUTH SERVICE FAILED", "failed to fetch configuration")
-                    serviceConfig = serviceConfiguration!!
-                    return@RetrieveConfigurationCallback
-                }
-
-                // use serviceConfiguration as needed
-
-            })
-
- */
-
-        //if(serviceConfig != null) {
-            authState = AuthState(serviceConfig)
-        //}
-
-        findViewById<Button>(R.id.button).setOnClickListener {
-            val authRequestBuilder = AuthorizationRequest.Builder(
-                serviceConfig,  // the authorization service configuration
-                MY_CLIENT_ID,  // the client ID, typically pre-registered and static
-                ResponseTypeValues.CODE,  // the response_type value: we want a code
-                MY_REDIRECT_URI
-            ) // the redirect URI to which the auth response is sent
-
-            val authRequest = authRequestBuilder
-                .setScope("wallet:user:read wallet:accounts:read")
-                .setLoginHint("jdoe@user.example.com")
-                .build()
-
-            authService = AuthorizationService(this)
-
-            //authService.performAuthorizationRequest(
-            //  authRequest,
-            //  PendingIntent.getActivity(this, 0, Intent(this, Test::class.java), 0),
-            //PendingIntent.getActivity(this, 0, Intent(this, Test::class.java), 0)
-            // )
-            doAuthorization(authRequest, 100)
-
-
-
-
-
-        }
-
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://www.coinbase.com/oauth/authorize?client_id=e4faf6ec45843a2f1e8a42c6242f3d8e82ce5603d3ee9c86c85be29a6361104f&redirect_uri=cryptowallet%3A%2F%2Fcallback&response_type=code&scope=wallet%3Auser%3Aread")
+        )
+        startActivity(intent)
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val resp = AuthorizationResponse.fromIntent(intent)
-        val ex = AuthorizationException.fromIntent(intent)
-        if (resp != null) {
-            Log.e("Test","$resp")
-            // authorization completed
-        } else {
-            Log.e("Test2","${ex?.cause}")
-            // authorization failed, check ex for more details
+    override fun onResume() {
+        super.onResume()
+        val uri = intent.data
+        if(uri != null){
+            val code = uri.getQueryParameter("code")
+            //cryptowallet://callback?code=e260bd24659d7ee7c88acf1550839ded1a90b159d4cb37d24fd208df4a4222fb
         }
-        // ...
-    }
-    private fun doAuthorization(authRequest:AuthorizationRequest,RC_AUTH:Int) {
-        val authService = AuthorizationService(this)
-        val authIntent = authService.getAuthorizationRequestIntent(authRequest)
-        startActivityForResult(authIntent, RC_AUTH)
 
     }
 
 }
+

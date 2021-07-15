67a01c6b49b85c4d322e2225dd529eaf22ebd5a2
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-07-15 00:35:50 -0500

    cleaning up code

--- app/src/main/java/com/example/cryptowallet/MainActivity.kt
+++ app/src/main/java/com/example/cryptowallet/MainActivity.kt
@@ -2,17 +2,12 @@ package com.example.cryptowallet
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.net.toUri
-import androidx.core.widget.TextViewOnReceiveContentListener
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cryptowallet.databinding.ActivityMainBinding
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.squareup.moshi.Moshi
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val MY_CLIENT_ID = "e4faf6ec45843a2f1e8a42c6242f3d8e82ce5603d3ee9c86c85be29a6361104f"
         const val CLIENT_SECRET = "84e2a6a63dc56f5d5be617f77cf71c0c4069a7e3a49cb9e2ce7e5d2bddea007f"
-        val MY_REDIRECT_URI = "cryptowallet://callback"
+        const val MY_REDIRECT_URI = "cryptowallet://callback"
         var accessToken:AccessToken?=null
     }
 
@@ -40,34 +35,29 @@ class MainActivity : AppCompatActivity() {
             )
             startActivity(intent)
         }
-
-
-
     }
 
     override fun onResume() {
         super.onResume()
-        var uri = intent.data
+        val uri = intent.data
         if(uri != null){
-            var code = uri.getQueryParameter("code")!!
+            val code = uri.getQueryParameter("code")!!
             //cryptowallet://callback?code=e260bd24659d7ee7c88acf1550839ded1a90b159d4cb37d24fd208df4a4222fb
-
-            var retrofitBuilder = Retrofit.Builder()
+            val retrofitBuilder = Retrofit.Builder()
                 .baseUrl("https://api.coinbase.com/")
                 .addConverterFactory(GsonConverterFactory.create())
-            var retrofit = retrofitBuilder.build()
-            var coinBaseClient = retrofit.create(CoinBaseClient::class.java)
-            var accessTokenCall = coinBaseClient.getToken(
+            val retrofit = retrofitBuilder.build()
+            val coinBaseClient = retrofit.create(CoinBaseClient::class.java)
+            val accessTokenCall = coinBaseClient.getToken(
                 "authorization_code",
                 code,
                 MY_CLIENT_ID,
                 CLIENT_SECRET,
                 MY_REDIRECT_URI
-
             )
             accessTokenCall.enqueue(object: Callback<AccessToken> {
                 override fun onResponse(call: Call<AccessToken>, response: Response<AccessToken>) {
-                    Toast.makeText(this@MainActivity,"good response: ${response.isSuccessful}",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity,"good response: ${response.body()?.access_token}",Toast.LENGTH_SHORT).show()
                     accessToken= AccessToken(
                         response.body()?.access_token!!,
                         response.body()?.token_type!!,
@@ -78,12 +68,10 @@ class MainActivity : AppCompatActivity() {
                     findViewById<TextView>(R.id.text_test).text = accessToken!!.access_token
 
                 }
-
                 override fun onFailure(call: Call<AccessToken>, t: Throwable) {
                     Toast.makeText(this@MainActivity,"bad response",Toast.LENGTH_SHORT).show()
                 }
             })
         }
     }
-
 }
